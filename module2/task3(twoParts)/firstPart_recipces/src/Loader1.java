679060333f55178c4a77628f8e2328e19b331d56
Znakarik <znakarik@icloud.com>
2020-01-16 04:51:03 +0300

    modified module 3 task3 recipies

--- module2/task3(twoParts)/firstPart_recipces/src/Loader1.java
+++ module2/task3(twoParts)/firstPart_recipces/src/Loader1.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Loader1 {
 
     public static void main(String[] args) {
-        int milkAmount = 1000; // ml
+        int milkAmount = 0; // ml
         int powderAmount = 1000; // g
         int eggsCount = 1000; // items
         int sugarAmount = 1000; // g
@@ -14,27 +14,40 @@ public class Loader1 {
         String youCan = "You can cook ";
 
         boolean canCookSmth = false;
+        boolean[] list = new boolean[3];
+
+        boolean canCookPancakes = false;
+        boolean canCookOmelette = false;
+        boolean canCookApplePie = false;
+
+        int tmp = 0;
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
         if ((milkAmount >= 1000) && (sugarAmount >= 10) && (powderAmount >= 400) && (oilAmount >= 30)) {
             System.out.println(youCan + "Pancakes");
-            canCookSmth = true;
+            list[0] = canCookPancakes = true;
         }
 
         //milk - 300 ml, powder - 5 g, eggs - 5
         if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
             System.out.println(youCan + "Omelette");
-            canCookSmth = true;
+            list[1] = canCookOmelette = true;
+        }
 
-            //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-            if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4) {
-                System.out.println(youCan + "Apple pie");
-                canCookSmth = true;
-            }
+        //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4) {
+            System.out.println(youCan + "Apple pie");
+            list[2] = canCookApplePie = true;
+        }
 
-            if (!canCookSmth) {
-                System.out.println("ничего нельзя приготовить");
+        for (boolean b : list) {
+            if (!b) {
+                tmp++;
             }
         }
+        if (tmp >= 3) {
+            System.out.println("ничего нельзя приготовить");
+        }
+
     }
 }
--- module3/Cat_2/src/Loader3.java
+++ module3/Cat_2/src/Loader3.java
@@ -1,54 +1,65 @@
 
 public class Loader3 {
     public static void main(String[] args) {
-        Cat2 cat = new Cat2();
-        Cat2 cat2 = new Cat2();
-
-        /*
-        Насчет Вашего комментария к заданию.
-        У нас же есть только конструктор без параметров,
-        Вы пытаетесь создать кота с параметром "0"
-        Создание невозможно, только если добавить новый конструктор с параметром int
-        Возможно я неправильно что-то поняла
-
-        Cat cat = new Cat(0);
-        Cat cat2 = new Cat();
-        for (int i = 0; i < cat.getWeight(); i++) {
-            cat2.meow();
-        }
-        System.out.println(cat2.getStatus()); // Playing
-         */
-
-        cat.feed(5000.);
-        System.out.println(cat.getStatus());
-
-        // Задание 2
-        System.out.println(cat.getAteFood());
-        System.out.println("Вес второй кошки до - " + cat2.getWeight());
-        cat2.goToPeepee();
-        System.out.println("Вес второй кошки после - " + cat2.getWeight());
-
-        //// Задание 4 - генерация кошки
-        System.out.println(Cat2.getCount());
-        Cat2 cat23 = Cat2.generateCatWithWeight(1000);
-        System.out.println(cat23.getWeight());
-
-        cat.setCatColors(CatColors.DARK_BLUE);
-
-        cat2.setCatColors(CatColors.GREEN);
-        System.out.println("Второй коть - " + cat2.getCatColors());
-        System.out.println("Первый коть - " + cat.getCatColors());
-
-        // Создание копии кошки
-        Cat2 cat24 = Cat2.makeCopyCat(cat2);
-        System.out.println("Цвет нашей скопированной кошки должен быть одинаков с второй кошкой - "
-                + cat24.getCatColors());
+//        Cat2 cat = new Cat2();
+//        Cat2 cat2 = new Cat2();
+//
+////        ==========
+//
+//        Cat2 cat22 = new Cat2(0);
+//
+//        for (int i = 0; i < cat22.getWeight(); i++) {
+//            cat22.meow();
+//        }
+//        System.out.println(cat22.getStatus()); // Playing
+//
+////        ==========
+//
+//        cat.feed(5000.);
+//        System.out.println(cat.getStatus());
+//
+//        // Задание 2
+//        System.out.println(cat.getAteFood());
+//        System.out.println("Вес второй кошки до - " + cat2.getWeight());
+//        cat2.goToPeepee();
+//        System.out.println("Вес второй кошки после - " + cat2.getWeight());
+//
+//        //// Задание 4 - генерация кошки
+//        System.out.println(Cat2.getCount());
+//        Cat2 cat23 = Cat2.generateCatWithWeight(1000);
+//        System.out.println(cat23.getWeight());
+//
+//        cat.setCatColors(CatColors.DARK_BLUE);
+//
+//        cat2.setCatColors(CatColors.GREEN);
+//        System.out.println("Второй коть - " + cat2.getCatColors());
+//        System.out.println("Первый коть - " + cat.getCatColors());
+//
+//        // Создание копии кошки
+//        Cat2 cat24 = Cat2.makeCopyCat(cat2);
+//        System.out.println("Цвет нашей скопированной кошки должен быть одинаков с второй кошкой - "
+//                + cat24.getCatColors());
 
         // Задание 1
 //        for (int i = 0; i < cat.getWeight(); i++) {
 //            cat2.meow();
 //        }
 //        System.out.println(cat2.getStatus());
+
+//        Cat2 c = new Cat2();
+//        System.out.println(c.getWeight());
+//        c.meow();
+//        System.out.println(c.getWeight());
+//        System.out.println(c.getAteFood());
+
+        Cat2 cat2 = new Cat2();
+
+        System.out.println(cat2.getWeight());
+        for (int i = 0; i < cat2.getWeight(); i++) {
+            cat2.feed(100000.);
+            System.out.println(cat2.getStatus());
+        }
+
     }
 
 }
